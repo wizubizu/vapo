8bac15cbed8e8ca16de00375c76877f081209994
wizubizu <odugbesanabimbola@gmail.com>
2018-06-20 16:52:05 +0100

    traveller dashboard, traveller message dashboard and traveller upload dashboard was created

--- assets/js/main.js
+++ assets/js/main.js
@@ -450,6 +450,216 @@ var appz = {
      //end
 
 
+
+// traveller file upload
+
+    // uplaod 0
+
+    var count=0;
+        function handleFileSelect(evt) {
+            var $fileUpload = $("input#files[type='file']");
+            count=count+parseInt($fileUpload.get(0).files.length);
+            
+            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
+                alert("You can only upload a maximum of 5 files");
+                count=count-parseInt($fileUpload.get(0).files.length);
+                evt.preventDefault();
+                evt.stopPropagation();
+                return false;
+            }
+            var files = evt.target.files;
+            for (var i = 0, f; f = files[i]; i++) {
+                if (!f.type.match('image || pdf || docx' )) {
+                    continue;
+                }
+                var reader = new FileReader();
+
+                reader.onload = (function (theFile) {
+                    return function (e) {
+                        var span = document.createElement('span');
+                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
+                        document.getElementById('list').insertBefore(span, null);
+                    };
+                })(f);
+
+                reader.readAsDataURL(f);
+            }
+        }
+        
+        $('#files').change(function(evt){
+            handleFileSelect(evt);
+        });  
+
+        $('#list').on('click', '.remove_img_preview',function () {
+            $(this).parent('span').remove();
+            
+            //this is not working...
+            var i = array.indexOf($(this));
+            if(i != -1) {
+                array.splice(i, 1);
+            }
+            // tried this too:
+            //$(this).parent('span').splice( 1, 1 );
+            
+            count--;
+        });
+
+        // upload 1
+
+    var count=0;
+
+        function handleFileSelect(evt) {
+            var $fileUpload = $("input#files[type='file']");
+            count=count+parseInt($fileUpload.get(0).files.length);
+            
+            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
+                alert("You can only upload a maximum of 5 files");
+                count=count-parseInt($fileUpload.get(0).files.length);
+                evt.preventDefault();
+                evt.stopPropagation();
+                return false;
+            }
+            var files = evt.target.files;
+            for (var i = 0, f; f = files[i]; i++) {
+                if (!f.type.match('image || pdf || docx' )) {
+                    continue;
+                }
+                var reader = new FileReader();
+
+                reader.onload = (function (theFile) {
+                    return function (e) {
+                        var span = document.createElement('span');
+                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
+                        document.getElementById('list').insertBefore(span, null);
+                    };
+                })(f);
+
+                reader.readAsDataURL(f);
+            }
+        }
+        
+        $('#files1').change(function(evt){
+            handleFileSelect(evt);
+        });  
+
+        $('#list1').on('click', '.remove_img_preview',function () {
+            $(this).parent('span').remove();
+            
+            //this is not working...
+            var i = array.indexOf($(this));
+            if(i != -1) {
+                array.splice(i, 1);
+            }
+            // tried this too:
+            //$(this).parent('span').splice( 1, 1 );
+            
+            count--;
+        });
+
+    // upload 2
+
+    var count=0;
+
+        function handleFileSelect(evt) {
+            var $fileUpload = $("input#files[type='file']");
+            count=count+parseInt($fileUpload.get(0).files.length);
+            
+            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
+                alert("You can only upload a maximum of 5 files");
+                count=count-parseInt($fileUpload.get(0).files.length);
+                evt.preventDefault();
+                evt.stopPropagation();
+                return false;
+            }
+            var files = evt.target.files;
+            for (var i = 0, f; f = files[i]; i++) {
+                if (!f.type.match('image || pdf || docx' )) {
+                    continue;
+                }
+                var reader = new FileReader();
+
+                reader.onload = (function (theFile) {
+                    return function (e) {
+                        var span = document.createElement('span');
+                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
+                        document.getElementById('list').insertBefore(span, null);
+                    };
+                })(f);
+
+                reader.readAsDataURL(f);
+            }
+        }
+        
+        $('#files2').change(function(evt){
+            handleFileSelect(evt);
+        });  
+
+        $('#list2').on('click', '.remove_img_preview',function () {
+            $(this).parent('span').remove();
+            
+            //this is not working...
+            var i = array.indexOf($(this));
+            if(i != -1) {
+                array.splice(i, 1);
+            }
+            // tried this too:
+            //$(this).parent('span').splice( 1, 1 );
+            
+            count--;
+        });
+
+        // upload 3
+
+    var count=0;
+        function handleFileSelect(evt) {
+            var $fileUpload = $("input#files[type='file']");
+            count=count+parseInt($fileUpload.get(0).files.length);
+            
+            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
+                alert("You can only upload a maximum of 5 files");
+                count=count-parseInt($fileUpload.get(0).files.length);
+                evt.preventDefault();
+                evt.stopPropagation();
+                return false;
+            }
+            var files = evt.target.files;
+            for (var i = 0, f; f = files[i]; i++) {
+                if (!f.type.match('image || pdf || docx' )) {
+                    continue;
+                }
+                var reader = new FileReader();
+
+                reader.onload = (function (theFile) {
+                    return function (e) {
+                        var span = document.createElement('span');
+                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
+                        document.getElementById('list').insertBefore(span, null);
+                    };
+                })(f);
+
+                reader.readAsDataURL(f);
+            }
+        }
+        
+        $('#files3').change(function(evt){
+            handleFileSelect(evt);
+        });  
+
+        $('#list3').on('click', '.remove_img_preview',function () {
+            $(this).parent('span').remove();
+            
+            //this is not working...
+            var i = array.indexOf($(this));
+            if(i != -1) {
+                array.splice(i, 1);
+            }
+            // tried this too:
+            //$(this).parent('span').splice( 1, 1 );
+            
+            count--;
+        });
+
+    // end
     
 
     
