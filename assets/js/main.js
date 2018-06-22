cc1d8ff2d7db900541ae3873a2c647c2b5a75182
wizubizu <odugbesanabimbola@gmail.com>
2018-06-22 15:30:23 +0100

    update on all the files and traveller upload dashboard was created

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,7 +82,7 @@ var appz = {
 
  
 
-// kowa js///
+// vapo js///
     app.imageHandler();
 
     $("[data-toggle=popover]").popover({
@@ -102,20 +102,16 @@ var appz = {
     });
 
     // accordion
-    var acc = document.getElementsByClassName("__accordion");
-    var i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-            panel.style.maxHeight = null;
-        } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            } 
+    $(document).ready(function() {
+    $('.__accordion').find('.accordion-toggle').click(function() {
+        $(this).next().slideToggle('600');
+        $(".accordion-content").not($(this).next()).slideUp('600');
         });
-    }
+        $('.accordion-toggle').on('click', function() {
+            $(this).toggleClass('active').siblings().removeClass('active');
+        });
+    });
 
     //fileupload for both
         $('input[id=base-input5]').change(function() {
@@ -451,217 +447,79 @@ var appz = {
 
 
 
-// traveller file upload
+    // traveller file upload
 
-    // uplaod 0
-
-    var count=0;
-        function handleFileSelect(evt) {
-            var $fileUpload = $("input#files[type='file']");
-            count=count+parseInt($fileUpload.get(0).files.length);
-            
-            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
-                alert("You can only upload a maximum of 5 files");
-                count=count-parseInt($fileUpload.get(0).files.length);
-                evt.preventDefault();
-                evt.stopPropagation();
-                return false;
-            }
-            var files = evt.target.files;
-            for (var i = 0, f; f = files[i]; i++) {
-                if (!f.type.match('image || pdf || docx' )) {
-                    continue;
-                }
-                var reader = new FileReader();
-
-                reader.onload = (function (theFile) {
-                    return function (e) {
-                        var span = document.createElement('span');
-                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
-                        document.getElementById('list').insertBefore(span, null);
-                    };
-                })(f);
-
-                reader.readAsDataURL(f);
-            }
-        }
-        
-        $('#files').change(function(evt){
-            handleFileSelect(evt);
-        });  
-
-        $('#list').on('click', '.remove_img_preview',function () {
-            $(this).parent('span').remove();
-            
-            //this is not working...
-            var i = array.indexOf($(this));
-            if(i != -1) {
-                array.splice(i, 1);
-            }
-            // tried this too:
-            //$(this).parent('span').splice( 1, 1 );
-            
-            count--;
-        });
-
-        // upload 1
-
-    var count=0;
-
-        function handleFileSelect(evt) {
-            var $fileUpload = $("input#files[type='file']");
-            count=count+parseInt($fileUpload.get(0).files.length);
-            
-            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
-                alert("You can only upload a maximum of 5 files");
-                count=count-parseInt($fileUpload.get(0).files.length);
-                evt.preventDefault();
-                evt.stopPropagation();
-                return false;
-            }
-            var files = evt.target.files;
-            for (var i = 0, f; f = files[i]; i++) {
-                if (!f.type.match('image || pdf || docx' )) {
-                    continue;
-                }
-                var reader = new FileReader();
-
-                reader.onload = (function (theFile) {
-                    return function (e) {
-                        var span = document.createElement('span');
-                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
-                        document.getElementById('list').insertBefore(span, null);
-                    };
-                })(f);
-
-                reader.readAsDataURL(f);
-            }
-        }
-        
-        $('#files1').change(function(evt){
-            handleFileSelect(evt);
-        });  
-
-        $('#list1').on('click', '.remove_img_preview',function () {
-            $(this).parent('span').remove();
-            
-            //this is not working...
-            var i = array.indexOf($(this));
-            if(i != -1) {
-                array.splice(i, 1);
-            }
-            // tried this too:
-            //$(this).parent('span').splice( 1, 1 );
-            
-            count--;
-        });
-
-    // upload 2
-
-    var count=0;
-
-        function handleFileSelect(evt) {
-            var $fileUpload = $("input#files[type='file']");
-            count=count+parseInt($fileUpload.get(0).files.length);
-            
-            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
-                alert("You can only upload a maximum of 5 files");
-                count=count-parseInt($fileUpload.get(0).files.length);
-                evt.preventDefault();
-                evt.stopPropagation();
-                return false;
-            }
-            var files = evt.target.files;
-            for (var i = 0, f; f = files[i]; i++) {
-                if (!f.type.match('image || pdf || docx' )) {
-                    continue;
-                }
-                var reader = new FileReader();
-
-                reader.onload = (function (theFile) {
-                    return function (e) {
-                        var span = document.createElement('span');
-                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
-                        document.getElementById('list').insertBefore(span, null);
-                    };
-                })(f);
-
-                reader.readAsDataURL(f);
-            }
-        }
-        
-        $('#files2').change(function(evt){
-            handleFileSelect(evt);
-        });  
-
-        $('#list2').on('click', '.remove_img_preview',function () {
-            $(this).parent('span').remove();
-            
-            //this is not working...
-            var i = array.indexOf($(this));
-            if(i != -1) {
-                array.splice(i, 1);
-            }
-            // tried this too:
-            //$(this).parent('span').splice( 1, 1 );
-            
-            count--;
-        });
-
-        // upload 3
-
-    var count=0;
-        function handleFileSelect(evt) {
-            var $fileUpload = $("input#files[type='file']");
-            count=count+parseInt($fileUpload.get(0).files.length);
-            
-            if (parseInt($fileUpload.get(0).files.length) > 6 || count>5) {
-                alert("You can only upload a maximum of 5 files");
-                count=count-parseInt($fileUpload.get(0).files.length);
-                evt.preventDefault();
-                evt.stopPropagation();
-                return false;
-            }
-            var files = evt.target.files;
-            for (var i = 0, f; f = files[i]; i++) {
-                if (!f.type.match('image || pdf || docx' )) {
-                    continue;
-                }
-                var reader = new FileReader();
-
-                reader.onload = (function (theFile) {
-                    return function (e) {
-                        var span = document.createElement('span');
-                        span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name), '"/><span class="remove_img_preview"></span>'].join('');
-                        document.getElementById('list').insertBefore(span, null);
-                    };
-                })(f);
-
-                reader.readAsDataURL(f);
-            }
-        }
-        
-        $('#files3').change(function(evt){
-            handleFileSelect(evt);
-        });  
-
-        $('#list3').on('click', '.remove_img_preview',function () {
-            $(this).parent('span').remove();
-            
-            //this is not working...
-            var i = array.indexOf($(this));
-            if(i != -1) {
-                array.splice(i, 1);
-            }
-            // tried this too:
-            //$(this).parent('span').splice( 1, 1 );
-            
-            count--;
-        });
-
-    // end
     
+    var fileTypes = ['pdf', 'docx', 'rtf', 'jpg', 'jpeg', 'png', 'txt'];  //acceptable file types
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var extension = input.files[0].name.split('.').pop().toLowerCase(),  //file extension from input file
+                isSuccess = fileTypes.indexOf(extension) > -1;  //is extension in acceptable types
 
+            if (isSuccess) { //yes
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    if (extension == 'pdf'){
+                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/179/179483.svg');
+                    }
+                    else if (extension == 'docx'){
+                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/281/281760.svg');
+                    }
+                    else if (extension == 'rtf'){
+                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136539.svg');
+                    }
+                    else if (extension == 'png'){ $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136523.svg'); 
+                    }
+                    else if (extension == 'jpg' || extension == 'jpeg'){
+                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136524.svg');
+                    }
+                  else if (extension == 'txt'){
+                        $(input).closest('.fileUpload').find(".icon").attr('src','https://image.flaticon.com/icons/svg/136/136538.svg');
+                    }
+                    else {
+                        //console.log('here=>'+$(input).closest('.uploadDoc').length);
+                        $(input).closest('.uploadDoc').find(".docErr").slideUp('slow');
+                    }
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+            else {
+                    //console.log('here=>'+$(input).closest('.uploadDoc').find(".docErr").length);
+                $(input).closest('.uploadDoc').find(".docErr").fadeIn();
+                setTimeout(function() {
+                        $('.docErr').fadeOut('slow');
+                        }, 9000);
+            }
+        }
+    }
+    $(document).ready(function(){
+       
+       $(document).on('change','.up', function(){
+        var id = $(this).attr('id'); /* gets the filepath and filename from the input */
+           var profilePicValue = $(this).val();
+           var fileNameStart = profilePicValue.lastIndexOf('\\'); /* finds the end of the filepath */
+           profilePicValue = profilePicValue.substr(fileNameStart + 1).substring(0,20); /* isolates the filename */
+           //var profilePicLabelText = $(".upl"); /* finds the label text */
+           if (profilePicValue != '') {
+            //console.log($(this).closest('.fileUpload').find('.upl').length);
+              $(this).closest('.fileUpload').find('.upl').html(profilePicValue); /* changes the label text */
+           }
+       });
+
+       $(".btn-new").on('click',function(){
+            $("#uploader").append('<div class="row uploadDoc"><div class="col-sm-11 col-xs-10"><div class="docErr">Please upload valid file</div><!--error--><div class="fileUpload btn btn-orange"> <img src="https://image.flaticon.com/icons/svg/136/136549.svg" class="icon"><span class="upl" id="upload">Upload document</span><input type="file" class="upload up" id="up" onchange="readURL(this);" /></div></div><div class="col-sm-1 col-xs-2"><a class="btn-check"><i class="fa fa-times"></i></a></div></div>');
+       });
+        
+       $(document).on("click", "a.btn-check" , function() {
+         if($(".uploadDoc").length>1){
+            $(this).closest(".uploadDoc").remove();
+          }else{
+            alert("You have to upload at least one document.");
+          } 
+       });
+    });
+    // end
     
 
 
