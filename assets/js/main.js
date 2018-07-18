266e129873d7d541e18a56f9c2ada65e9bdea2a3
wizubizu <odugbesanabimbola@gmail.com>
2018-07-18 16:59:24 +0100

    traveller_activites, and traveller_payment created

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,36 +79,7 @@ var appz = {
     
 }
 
-// chat upload img
-var app = {
- imageHandler:function() {
-        $("#upload2").on('change', function() {
-            //alert("alert");
-            readUrl();
-        });
-        function readUrl (){
-            var file = $("#upload2")[0].files[0];
-            //console.log(file);
-            //reader interprets the file selected
-            var reader = new FileReader();
-                //console.log(reader.result);
-            reader.onloadend = function (){
-                //console.log(reader.result);
-                $("#picture_previewup").attr("src", reader.result);
-                //$("#picture_preview")[0].src = "" + reader.results;
-                //console.log($("#picture_preview"));
-                //console.log(reader.result);
-            }
-            if(file){
-                reader.readAsDataURL(file);
-            }
-        }
-    }//end of imageHandler
 
-}
-
-
-// chat upload img end
 
 
  
